982efa36e80cc3f419383c06daafb4d30e89ecfe
Ricki Bin Yamin <ricki.bin.yamin@gmail.com>
2020-07-23 11:34:42 +0700

    Initial Commit

---
+++ SignUp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignUp
+//
+//  Created by Ricki Bin Yamin on 23/07/20.
+//  Copyright © 2020 Ricki Bin Yamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
